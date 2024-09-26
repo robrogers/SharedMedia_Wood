0d74fee260149edd40b5a6665ef0002ee63ebacb
Rob Rogers <rob.rogers@gmail.com>
2024-09-26 08:56:02 -0400

    initial push
    initial push

---
+++ Soundpaths.lua
@@ -0,0 +1,47 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFFFF00001. CarelessWhisper|r", [[Interface\Addons\SharedMedia_Wood\sound\carelesswhisper.ogg]])
+LSM:Register("sound", "|cFFFF00001. deeznutz|r", [[Interface\Addons\SharedMedia_Wood\sound\deeznutz.ogg]])
+LSM:Register("sound", "|cFFFF00001. fart|r", [[Interface\Addons\SharedMedia_Wood\sound\fart.ogg]])
+LSM:Register("sound", "|cFFFF00001. gta_wasted|r", [[Interface\Addons\SharedMedia_Wood\sound\gta_wasted.ogg]])
+LSM:Register("sound", "|cFFFF00001. mario_death|r", [[Interface\Addons\SharedMedia_Wood\sound\mario_death.ogg]])
+LSM:Register("sound", "|cFFFF00001. sad violin|r", [[Interface\Addons\SharedMedia_Wood\sound\sad violin.ogg]])
+LSM:Register("sound", "|cFFFF00001. tacobell|r", [[Interface\Addons\SharedMedia_Wood\sound\tacobell.ogg]])
+LSM:Register("sound", "|cFFFF00001. wilhelmscream|r", [[Interface\Addons\SharedMedia_Wood\sound\wilhelmscream.ogg]])
+LSM:Register("sound", "|cFFFF00001. SafetyDance|r", [[Interface\Addons\SharedMedia_Wood\sound\safetydance.ogg]])
+LSM:Register("sound", "|cFFFF00001. BabyGotBack|r", [[Interface\Addons\SharedMedia_Wood\sound\babygotback.ogg]])
+LSM:Register("sound", "|cFFFF00001. Toxic|r", [[Interface\Addons\SharedMedia_Wood\sound\bstoxic.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
